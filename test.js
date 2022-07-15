83d4940e32c8ab7ec4078499e244d66372f6fd89
wjddms4107 <78889402+wjddms4107@users.noreply.github.com>
2022-07-15 10:45:04 +0900

    test: even함수 수정

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 function test(num) {
   let even = [];
-  if (num % 2 === 0) {
-    even.push(num);
-  } return even;
+  for(let i = 2; i <=50; i++) {
+    if( i % 2 === 0) {
+      even.push(i);
+    }
+  }  return even;
 }
 
 test()
